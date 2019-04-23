24920448094ba74b67f08c2b8b61f50f253732c0
wuyanzu <1311470691@qq.com>
2019-04-23 16:51:36 +0800

    debug

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -15,17 +15,20 @@
 		    signature: "{{$sdk_config['signature']}}",// 必填，签名
 		    jsApiList: ['chooseImage','uploadImage'] // 必填，需要使用的JS接口列表
 		});
-		// console.log("{{$sdk_config['signature']}}");
-		// wx.ready(function(){
-		//     wx.chooseImage({
-		// 		count: 5, // 默认9
-		// 		sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-		// 		sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-		// 		success: function (res) {
-		// 		var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-		// 		}
-		// 	});
-		// });
+		console.log("{{$sdk_config['signature']}}");
+		wx.ready(function(){
+			$('#img').click(function(){
+				wx.chooseImage({
+					count: 5, // 默认9
+					sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+					sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+					success: function (res) {
+						var localIds = res.localIds; // 
+					}
+				});
+			})
+		    
+		});
 	</script>
 </body>
 </html>
