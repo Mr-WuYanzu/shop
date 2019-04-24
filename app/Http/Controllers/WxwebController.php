078772752f8d8223b3c6b6ddc78c8f454cd86b62
wuyanzu <1311470691@qq.com>
2019-04-24 18:49:16 +0800

    微信授权回复信息

--- app/Http/Controllers/WxwebController.php
+++ app/Http/Controllers/WxwebController.php
@@ -3,16 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\model\User;
 
 class WxwebController extends Controller
 {
     public function hd(){
    	  $code=$_GET['code'];
    	  $access_token=json_decode(file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxe5ff29e2590e9cef&secret=02f770d9872fdf95de605f22c783fe46&code=".$code."&grant_type=authorization_code"),true);
-   	  echo "<pre>";print_r($access_token);echo "</pre>";
+   	  // echo "<pre>";print_r($access_token);echo "</pre>";
    	  //用户信息
    	  $url='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token['access_token'].'&openid='.$access_token['openid'].'&lang=zh_CN';
    	  $userInfo=json_decode(file_get_contents($url),true);
-	  echo "<pre>";print_r($userInfo);echo "</pre>";
+	  // echo "<pre>";print_r($userInfo);echo "</pre>";
+	  // 用户信息入库
+	  $openid=$userInfo['openid'];
+	  $res=User::where('openid',$openid)->first();
+	  if($res){
+	  	$data=[
+		  	'openid'=>$openid,
+		  	'user_name'=>$userInfo['nickname'],
+		  	'user_sex'=>$userInfo['sex'],
+		  	'user_coutry'=>$userInfo['coutry'],
+		  	'user_province'=>$userInfo['province'],
+		  	'user_city'=>$userInfo['city'],
+		  	'headimgurl'=>$userInfo['headimgurl']
+		  ];
+	   	  User::insert($data);
+	   	  echo '欢迎:'.$userInfo['nickname'];
+	  }else{
+	  		echo '欢迎回来:'.$res->user_name;
+	  }
+	  
     }
 }
---
+++ app/model/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table="p_wx_user";
+    public $timestamps = false;
+}
