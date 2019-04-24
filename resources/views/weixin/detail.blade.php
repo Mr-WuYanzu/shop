d0e622a4acb2bfe03b595f9f56556b24664d6536
wuyanzu <1311470691@qq.com>
2019-04-24 10:43:24 +0800

    debug

--- resources/views/weixin/detail.blade.php
+++ resources/views/weixin/detail.blade.php
@@ -33,8 +33,8 @@
 		        wx.updateAppMessageShareData({
 		            title: '哈哈', // 分享标题
 		            desc: 'dd', // 分享描述
-		            link: 'http://1809zhanghaibo.comcto.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-		            imgUrl: 'http://1809abc.comcto.com/img/link.jpg', // 分享图标
+		            link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		            imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
 		            success: function () {
 		                alert('分享成功');
 		            }
