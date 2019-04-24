5eb33ec3ce1cadd13f1c6fd3bfff426e553a6fc3
wuyanzu <1311470691@qq.com>
2019-04-24 11:01:27 +0800

    debug

--- resources/views/weixin/detail.blade.php
+++ resources/views/weixin/detail.blade.php
@@ -30,17 +30,17 @@
 		    jsApiList: ['updateAppMessageShareData','onMenuShareAppMessage','onMenuShareTimeline'] // 必填，需要使用的JS接口列表
 		});
 		 wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
-		 	wx.onMenuShareAppMessage({
-				title: '分享给朋友', // 分享标题
-				desc: 'dd', // 分享描述
-				link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-				imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
-				type: 'link', // 分享类型,music、video或link，不填默认为link
-				dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-				success: function () {
-					alert('分享成功');
-				}
-			});
+		 // 	wx.onMenuShareAppMessage({
+			// 	title: '分享给朋友', // 分享标题
+			// 	desc: 'dd', // 分享描述
+			// 	link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			// 	imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
+			// 	type: 'link', // 分享类型,music、video或link，不填默认为link
+			// 	dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+			// 	success: function () {
+			// 		alert('分享成功');
+			// 	}
+			// });
 			wx.onMenuShareTimeline({
 			    title: '朋友圈', // 分享标题
 			    link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
