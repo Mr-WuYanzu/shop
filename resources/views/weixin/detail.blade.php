cf0137bf65042250e6ac8c29a73b15e13b7d5c9e
wuyanzu <1311470691@qq.com>
2019-04-24 10:55:51 +0800

    debug

--- resources/views/weixin/detail.blade.php
+++ resources/views/weixin/detail.blade.php
@@ -27,9 +27,29 @@
 		    timestamp: "{{$sdk_config['timestamp']}}", // 必填，生成签名的时间戳
 		    nonceStr: "{{$sdk_config['nonceStr']}}", // 必填，生成签名的随机串
 		    signature: "{{$sdk_config['signature']}}",// 必填，签名
-		    jsApiList: ['updateAppMessageShareData'] // 必填，需要使用的JS接口列表
+		    jsApiList: ['updateAppMessageShareData','onMenuShareAppMessage',''] // 必填，需要使用的JS接口列表
 		});
 		 wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+		 	wx.onMenuShareAppMessage({
+				title: '哈哈', // 分享标题
+				desc: 'dd', // 分享描述
+				link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+				imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
+				type: 'link', // 分享类型,music、video或link，不填默认为link
+				dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+				success: function () {
+					alert('分享成功');
+				}
+			});
+			wx.onMenuShareTimeline({
+			    title: '哈哈', // 分享标题
+			    link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			    imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
+			    success: function () {
+			    	// 用户点击了分享后执行的回调函数
+			    	alert('分享成功');
+				},
+			})
 		        wx.updateAppMessageShareData({
 		            title: '哈哈', // 分享标题
 		            desc: 'dd', // 分享描述
