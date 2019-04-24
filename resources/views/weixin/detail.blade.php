2eb1d49cd6cea6dcc1324f2e1a2cd1e70afd6b23
wuyanzu <1311470691@qq.com>
2019-04-24 11:35:00 +0800

    debug

--- resources/views/weixin/detail.blade.php
+++ resources/views/weixin/detail.blade.php
@@ -31,24 +31,7 @@
 		});
 		 wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
 		 	//分享给qq空间和朋友圈
-			wx.updateTimelineShareData({ 
-		        title: '朋友圈', // 分享标题
-		        link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-		        imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
-		        success: function () {
-		         	alert('分享成功');
-		        }
-		    })
-			//自定义分享到qq和朋友
-		        wx.updateAppMessageShareData({
-		            title: '哈哈', // 分享标题
-		            desc: 'dd', // 分享描述
-		            link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-		            imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
-		            success: function () {
-		               
-		            }
-		        })
+			
 		    });
 	</script>
 </body>
