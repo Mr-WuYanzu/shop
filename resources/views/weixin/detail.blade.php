4a0b0e886dcc4d5047a278e0ddc6aaeebecf676c
wuyanzu <1311470691@qq.com>
2019-04-24 13:47:39 +0800

    debug

--- resources/views/weixin/detail.blade.php
+++ resources/views/weixin/detail.blade.php
@@ -41,7 +41,7 @@
 		    });
 			//自定义分享到qq和朋友
 		        wx.updateAppMessageShareData({
-		            title: '哈哈', // 分享标题
+		            title: '哈哈哈', // 分享标题
 		            desc: 'dd', // 分享描述
 		            link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 		            imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
@@ -49,26 +49,26 @@
 		               
 		            }
 		        });
-		   wx.onMenuShareTimeline({
-			    title: '哈哈', // 分享标题
-			    link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-			    imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
-			    success: function () {
-			   		// 用户点击了分享后执行的回调函数
-				},
-			})
-		   wx.onMenuShareAppMessage({
-				title: 'hh', // 分享标题
-				desc: 'dd', // 分享描述
-				link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-				imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
-				type: 'link', // 分享类型,music、video或link，不填默认为link
-				dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-				success: function () {
-				// 用户点击了分享后执行的回调函数
-				}
-				});
-		});
+		//    wx.onMenuShareTimeline({
+		// 	    title: '哈哈', // 分享标题
+		// 	    link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		// 	    imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
+		// 	    success: function () {
+		// 	   		// 用户点击了分享后执行的回调函数
+		// 		},
+		// 	})
+		//    wx.onMenuShareAppMessage({
+		// 		title: 'hh', // 分享标题
+		// 		desc: 'dd', // 分享描述
+		// 		link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		// 		imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
+		// 		type: 'link', // 分享类型,music、video或link，不填默认为link
+		// 		dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+		// 		success: function () {
+		// 		// 用户点击了分享后执行的回调函数
+		// 		}
+		// 		});
+		// });
 	</script>
 </body>
 </html>
