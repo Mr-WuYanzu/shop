2e804313871cb13c8efde786039a946fb5e5b046
wuyanzu <1311470691@qq.com>
2019-04-24 10:02:03 +0800

    debug

--- resources/views/weixin/detail.blade.php
+++ resources/views/weixin/detail.blade.php
@@ -28,18 +28,18 @@
 		    timestamp: "{{$sdk_config['timestamp']}}", // 必填，生成签名的时间戳
 		    nonceStr: "{{$sdk_config['nonceStr']}}", // 必填，生成签名的随机串
 		    signature: "{{$sdk_config['signature']}}",// 必填，签名
-		    jsApiList: ['onMenuShareTimeline'] // 必填，需要使用的JS接口列表
+		    jsApiList: ['updateTimelineShareData'] // 必填，需要使用的JS接口列表
 		});
 		wx.ready(function(){
  			$('#share').click(function(){
- 				wx.onMenuShareTimeline({
-				    title: '小哥哥来呀', // 分享标题
-				    link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-				    imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
-				    success: function () {
-				    	alert('商品千万条，分享第一条。分享不规范，钱包两行泪。');
-					},
- 				})
+ 				wx.updateTimelineShareData({ 
+			        title: '小哥哥来呀', // 分享标题
+			        link: 'http://1809zhanghaibo.comcto.com/weixin/detail/?goods_id='+"{{$goodsInfo->goods_id}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			        imgUrl: 'http://1809zhanghaibo.comcto.com/img/link.jpg', // 分享图标
+			        success: function () {
+			          alert('商品千万条，分享第一条。分享不规范，钱包两行泪。');
+			        }
+			    })
 			});
  		});
 	</script>
