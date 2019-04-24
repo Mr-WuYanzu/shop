f77179105d74e87169dcba2a7308847686c38c5e
wuyanzu <1311470691@qq.com>
2019-04-24 09:21:24 +0800

    回复消息

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Uri;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+use DB;
 
 class WxController extends Controller
 {
@@ -16,7 +17,7 @@ class WxController extends Controller
     }
     //接受微信推送消息
     public function event(){
-    	$client=new Client();
+    	// $client=new Client();
         $data = file_get_contents("php://input");
         $time=date('Y-m-d H:i:s');
         $str=$time.$data."\n";
@@ -26,5 +27,29 @@ class WxController extends Controller
         $wx_id=$obj->ToUserName;
         $openid=$obj->FromUserName;
         $type=$obj->MsgType;
+        if($type=='text'){
+        	// echo "ss";
+        	// echo $obj->Content;
+        	if($obj->Content=="最新商品"){
+        		$data=DB::table('p_wx_goods')->orderBy('add_time','desc')->limit(5)->get();
+        		foreach($data as $k=>$v){
+        			echo '<xml>
+						  <ToUserName><![CDATA['.$openid.']]></ToUserName>
+						  <FromUserName><![CDATA['.$wx_id.']]></FromUserName>
+						  <CreateTime>'.time().'</CreateTime>
+						  <MsgType><![CDATA[news]]></MsgType>
+						  <ArticleCount>1</ArticleCount>
+						  <Articles>
+						    <item>
+						      <Title><![CDATA['.$v->goods_name.']]></Title>
+						      <Description><![CDATA['.$v->desc.']]></Description>
+						      <PicUrl><![CDATA['.'http://1809zhanghaibo.comcto.com/img'.']]></PicUrl>
+						      <Url><![CDATA[url]]></Url>
+						    </item>
+						  </Articles>
+						</xml>';
+        		}
+        	}
+        }
     }
 }
