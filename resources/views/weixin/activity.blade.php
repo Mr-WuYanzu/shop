c3e2b957e1c9e0cc6e27dce1bc8c0d80a2f2c506
wuyanzu <1311470691@qq.com>
2019-04-28 15:24:22 +0800

    回复视图

--- resources/views/weixin/activity.blade.php
+++ resources/views/weixin/activity.blade.php
@@ -17,12 +17,20 @@
             timestamp: "{{$sdk_config['timestamp']}}", // 必填，生成签名的时间戳
             nonceStr: "{{$sdk_config['nonceStr']}}", // 必填，生成签名的随机串
             signature: "{{$sdk_config['signature']}}",// 必填，签名
-            jsApiList: ['updateAppMessageShareData'] // 必填，需要使用的JS接口列表
+            jsApiList: ['updateAppMessageShareData','updateTimelineShareData'] // 必填，需要使用的JS接口列表
         });
         wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
             wx.updateAppMessageShareData({
                 title: '最新活动', // 分享标题
                 desc: '进来看看', // 分享描述
+                link: 'http://1809zhanghaibo.comcto.com/weixin/view', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://1809zhanghaibo.comcto.com/img/link (1).jpg', // 分享图标
+                success: function () {
+                    // 设置成功
+                }
+            });
+            wx.updateTimelineShareData({
+                title: '最新活动', // 分享标题
                 link: 'http://1809zhanghaibo.comcto.com/weixin/view', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                 imgUrl: 'http://1809zhanghaibo.comcto.com/img/link (1).jpg', // 分享图标
                 success: function () {
