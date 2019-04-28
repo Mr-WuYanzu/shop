6c6163c5adb26ad974c73495b66166e17038f9e8
wuyanzu <1311470691@qq.com>
2019-04-28 16:31:07 +0800

    回复视图

--- resources/views/weixin/activity.blade.php
+++ resources/views/weixin/activity.blade.php
@@ -17,7 +17,7 @@
             timestamp: "{{$sdk_config['timestamp']}}", // 必填，生成签名的时间戳
             nonceStr: "{{$sdk_config['nonceStr']}}", // 必填，生成签名的随机串
             signature: "{{$sdk_config['signature']}}",// 必填，签名
-            jsApiList: ['updateAppMessageShareData','updateTimelineShareData'] // 必填，需要使用的JS接口列表
+            jsApiList: ['updateAppMessageShareData','updateTimelineShareData','onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
         });
         wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
             wx.updateAppMessageShareData({
@@ -37,6 +37,30 @@
                     // 设置成功
                 }
             })
+
+
+
+            wx.onMenuShareTimeline({
+                title: '最新活动', // 分享标题
+                link: 'http://1809zhanghaibo.comcto.com/weixin/view', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://1809zhanghaibo.comcto.com/img/link (1).jpg', // 分享图标
+                success: function () {
+                    // 用户点击了分享后执行的回调函数
+                },
+            });
+
+            wx.onMenuShareAppMessage({
+                title: '最新活动', // 分享标题
+                desc: '来看一下', // 分享描述
+                link: 'http://1809zhanghaibo.comcto.com/weixin/view', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://1809zhanghaibo.comcto.com/img/link (1).jpg', // 分享图标
+                type: '', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () {
+// 用户点击了分享后执行的回调函数
+                }
+            });
+
         });
     </script>
 </body>
