c696db7ff2942e303ad75746fb5cde5845b40fbf
wuyanzu <1311470691@qq.com>
2019-04-29 09:55:17 +0800

    商品详情展示图片

--- resources/views/weixin/detail.blade.php
+++ resources/views/weixin/detail.blade.php
@@ -7,6 +7,7 @@
 	<div style="float:left">
 		<h1>商品详情</h1>
 		<p>商品名称:{{$goodsInfo->goods_name}}</p>
+        <p> 商品图片<img src="/img/{{$goodsInfo->goods_img}}" width="160"> </p>
 		<p>商品价格:{{$goodsInfo->goods_price}}</p>
 		<p>浏览次数：{{$history_num}}</p>
 	</div>
@@ -14,7 +15,10 @@
 		<h1>浏览历史</h1>
 		<ul>
 			@foreach($data as $k=>$v)
-			<li goods_id="{{$v->goods_id}}">商品名称>><a href="/weixin/detail?goods_id={{$v->goods_id}}">{{$v->goods_name}}</a>------价格>>{{$v->goods_price}} <input type="button" value="加入购物车" class="but"></li>
+			<li goods_id="{{$v->goods_id}}">
+                商品名称>><a href="/weixin/detail?goods_id={{$v->goods_id}}">{{$v->goods_name}}</a>
+                ------价格>>{{$v->goods_price}}
+                <input type="button" value="加入购物车" class="but"></li>
 			@endforeach
 
 		</ul>---------------------------------------------------------------------------
